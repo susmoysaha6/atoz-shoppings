a5528028e207062be77b53cccb2a9c1c2cdc0ebb
susmoysaha6 <susmoysaha6@gmail.com>
2023-01-17 12:36:03 +0600

    banner added

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import logo from '../assets/logo.png'
+// import logo from '../assets/logo.png'
+import Typewriter from 'typewriter-effect';
 
 const Banner = () => {
     return (
-        <div className='w-full min-h-[400px] bg-orange-600'>
-            <img src={logo} alt="" />
+        <div className='w-full min-h-[400px] bg-orange-600 grid grid-cols-1 md:grid-cols-2 gap-4 items-center justify-center '>
+            {/* <img src={logo} alt="" /> */}
+            <div className='text-3xl my-10 px-5 lg:px-20 w-full lg:mx-20'>
+                <h1 className=' mb-5'>Welcome to AtoZ</h1>
+                <h2 className=' mb-5'>We provide you a daily Solution</h2>
+                <Typewriter
+                    options={{
+                        strings: ['Laptop', 'Mobile', 'Lifestyle', 'Cosmetics', 'Grocery', 'Shoes', 'Anything You Want'],
+                        autoStart: true,
+                        loop: true,
+                    }}
+                />
+            </div>
+            <div className=' md:block'>
+                <img width="w-full" src="https://i.ibb.co/VV0R2q3/bp.png" alt="" />
+            </div>
         </div>
         // <div className='mb-10'>
         //     <div className="carousel w-full">
